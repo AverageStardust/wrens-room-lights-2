fbaa58b0bef720f40765abab103e7f60f06e8502
Wren Durbano <wren.durbano@gmail.com>
2023-11-03 15:12:54 -0600

    Made bootloader retry fetches

--- public/devicePayload/bootloader.ts
+++ public/devicePayload/bootloader.ts
@@ -7,32 +7,62 @@ const siteURL = "https://wrens-room-lights-2.web.app";
 async function main() {
 	// check versions
 	const successorMetaURL = path.join(siteURL, "./devicePayload/meta.json");
-	const successorMetaResponse = await fetch(successorMetaURL);
+	const successorMetaResponse = await resilientFetch(successorMetaURL);
 	const successorMeta = await successorMetaResponse.json() as typeof meta;
 
 	if (meta.version >= successorMeta.version) {
-		console.log(`Payload v${meta.version} is up to date`);
-
-		// open main
-		const succeed = new Deno.Command("bash", {
-			args: ["./startMain.sh"]
-		});
-		await succeed.output();
+		startMain();
 	} else {
-		console.log(`Updating payload v${meta.version} to v${successorMeta.version}`);
-
-		// download payload
-		const successorURL = path.join(siteURL, "./devicePayload.zip");
-		const successorResponse = await fetch(successorURL);
-		const successorArray = new Uint8Array(await successorResponse.arrayBuffer());
-		Deno.writeFile("../devicePayload.zip", successorArray, { create: true });
-
-		// replace self and start new
-		const succeed = new Deno.Command("bash", {
-			args: ["./succeed.sh", path.basename(Deno.cwd())]
-		});
-		await succeed.output();
+		updatePayload(successorMeta);
 	}
+}
+
+async function startMain() {
+	console.log(`Payload v${meta.version} is up to date`);
+
+	// open main
+	const succeed = new Deno.Command("bash", {
+		args: ["./startMain.sh"]
+	});
+	await succeed.output();
+}
+
+async function updatePayload(successorMeta: typeof meta) {
+	console.log(`Updating payload v${meta.version} to v${successorMeta.version}`);
+
+	// download payload
+	const successorURL = path.join(siteURL, "./devicePayload.zip");
+	const successorResponse = await resilientFetch(successorURL);
+	const successorArray = new Uint8Array(await successorResponse.arrayBuffer());
+	Deno.writeFile("../devicePayload.zip", successorArray, { create: true });
+
+	// replace self and start new
+	const succeed = new Deno.Command("bash", {
+		args: ["./succeed.sh", path.basename(Deno.cwd())]
+	});
+	await succeed.output();
+}
+
+async function resilientFetch(input: string | URL | Request, init?: RequestInit, tries = 5) {
+	let msDelay = 500;
+	let lastError;
+
+	while (tries > 0) {
+		try {
+			return await fetch(input, init);
+		} catch (error) {
+			lastError = error;
+		}
+		tries--;
+		delay(msDelay);
+		msDelay *= 2;
+	}
+
+	throw lastError;
+}
+
+function delay(msDelay: number) {
+	return new Promise((resolve) => setTimeout(resolve, msDelay));
 }
 
 main();
