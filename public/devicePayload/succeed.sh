9e950e34873fbafa35ff0169f663953fc80189c0
Wren Durbano <wren.durbano@gmail.com>
2023-11-03 15:40:33 -0600

    updated succeed.sh

--- public/devicePayload/succeed.sh
+++ public/devicePayload/succeed.sh
@@ -4,5 +4,5 @@ sudo  unzip newDevicePayload.zip -d newDevicePayload
 sudo rm -f newDevicePayload.zip
 sudo rm -r -f $1
 sudo mv newDevicePayload $1
-sudo chmod 777 $1
+sudo chmod -R 777 $1
 bash $1/start.sh
