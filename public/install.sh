8ce047414cb89af1e25979d5f7670203b8a858c8
Wren Durbano <25310577+AverageStardust@users.noreply.github.com>
2023-11-03 16:13:28 -0600

    Update install.sh

--- public/install.sh
+++ public/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl https://wrens-room-lights-2.web.app/devicePayload.zip -o devicePayload.zip
+rm -r -f roomDeivce
 unzip devicePayload.zip -d roomDeivce
 rm -f devicePayload.zip
 chmod 777 -R roomDeivce
