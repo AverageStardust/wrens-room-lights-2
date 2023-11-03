0c8083cd5f0044cd1a6dcad203d692061254c6de
Wren Durbano <wren.durbano@gmail.com>
2023-11-03 16:11:30 -0600

    updated installer

--- public/install.sh
+++ public/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 curl https://wrens-room-lights-2.web.app/devicePayload.zip -o devicePayload.zip
-unzip devicePayload.zip -d devicePayload
+unzip devicePayload.zip -d roomDeivce
 rm -f devicePayload.zip
+chmod 777 -R roomDeivce
